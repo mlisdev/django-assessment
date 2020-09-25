f8a026ead6a9534b8cdf81265784956732fcfc29
Stephanie Rinehart <rinehart@MacBook-Pro.local>
2020-09-25 14:19:27 -0400

    complete step 3.2

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+
 # Create your models here.
-# class Widget(model.Models):
-#     pass
+class Widget(models.Model):
+    description = models.CharField(max_length=240)
+    quantity = models.IntegerField()
